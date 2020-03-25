e59ba384cc6744550da86f21159d90097ccd6728
gyutae10002 <gyutae@manycoresoft.co.kr>
2020-03-25 19:24:05 +0900

    no message

---
+++ rtl-tdd-todos/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoApp from './Todos/TodoApp';
+
+function App() {
+  return <TodoApp />
+  
+}
+
+export default App;
