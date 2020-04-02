76e969ca41dc30986a6a52568f7eddd8951a349f
gyutae10002 <gyutae@manycoresoft.co.kr>
2020-04-02 18:20:25 +0900

    no message

---
+++ rtl-redux-refactoring-count/src/container/WaitingListContainer.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as waitingActions from '../store/modules/waiting';
+import WaitingList from '../components/WaitingList';
